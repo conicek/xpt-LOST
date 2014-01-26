27a1ff2ee3ff4d2f0503ea23989d35f44ee76e1f
conicek <brauer.f@seznam.cz>
2014-01-26 17:55:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -625,7 +625,7 @@ int main(int argc, char** argv)
 	xptMiner_parseCommandline(argc, argv);
 	minerSettings.protoshareMemoryMode = commandlineInput.ptsMemoryMode;
 	printf("\xC9\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xCD\xBB\n");
-	printf("\xBA  xptMiner (v1.0)Fast + Linux Ubuntu 13.04 x64    \xBA\n");
+	printf("\xBA  xptMiner (v1.0)Fast ++ Linux Ubuntu 13.04 x64   \xBA\n");
 	printf("\xBA  author: §§con§§                                 \xBA\n");
 	printf("\xBA  modify by con                                   \xBA\n");
 	printf("\xBA  Donation: MCkWVoQ5NeR8UFymjKyCVkSaEXjCTieGAv    \xBA\n");
